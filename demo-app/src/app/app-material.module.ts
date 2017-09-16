5e77f2277a2d9b28bbc9812d6eb18d38cf5be7bb
btroncone <btroncone@gmail.com>
2017-09-16 12:28:21 -0400

    toolbars - lesson code

--- demo-app/src/app/app-material.module.ts
+++ demo-app/src/app/app-material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MdToolbarModule } from '@angular/material';
+import { ScrollDispatchModule, VIEWPORT_RULER_PROVIDER } from '@angular/cdk/scrolling';
 
 @NgModule({
   exports: [
-    MdToolbarModule
-  ]
+    MdToolbarModule,
+    ScrollDispatchModule
+  ],
+  providers: [ VIEWPORT_RULER_PROVIDER ]
 })
 export class AppMaterialModule { }
--- demo-app/src/app/app.component.ts
+++ demo-app/src/app/app.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { ViewportRuler } from '@angular/cdk/scrolling';
 
+export const PRIMARY_TEXT_THRESHOLD = 34;
+export const PRIMARY_SHADOW_THRESHOLD = 100;
 @Component({
   selector: 'app-root',
   template: `
-  <md-toolbar color="primary">
-    Structuring Angular Apps with Angular Material Components
-  </md-toolbar>
+  <div class="app-content">
+      <md-toolbar color="primary" class="primary-header" [class.with-shadow]="applyShadow">
+        <span *ngIf="popText">Structuring Angular Apps with Angular Material Components</span>
+      </md-toolbar>
+      <md-toolbar color="primary" class="extended-header">
+        <span *ngIf="!popText">Material Design Toolbars</span>
+      </md-toolbar>
+  </div>
   `,
+  styleUrls: [ './app.component.scss' ]
 })
-export class AppComponent {}
+export class AppComponent {
+  @HostListener('window:scroll', ['$event']) private onScroll() {
+    this.determineHeader(this._viewportRuler.getViewportScrollPosition());
+  };
+
+  public popText : boolean;
+  public applyShadow : boolean;
+
+  constructor(
+    private _viewportRuler : ViewportRuler
+  ) {}
+
+  determineHeader = ({ top } : { top: number }) => {
+    if (top >= PRIMARY_TEXT_THRESHOLD) {
+      this.popText = true;
+    } else {
+      this.popText = false;
+    }
+
+    if (top >= PRIMARY_SHADOW_THRESHOLD) {
+      this.applyShadow = true;
+    } else {
+      this.applyShadow = false;
+    }
+  }
+}
