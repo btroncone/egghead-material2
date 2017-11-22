cc58de0e18ac9b3fb0cb580658ac3c51c968f050
btroncone <btroncone@gmail.com>
2017-11-22 05:16:47 -0500

    feat(inputs): examples for inputs lesson

--- src/app/lessons/inputs/inputs-lesson.module.ts
+++ src/app/lessons/inputs/inputs-lesson.module.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { MatInputModule } from '@angular/material';
+import {
+  MatInputModule,
+  MAT_PLACEHOLDER_GLOBAL_OPTIONS
+} from '@angular/material';
+
 import { InputsLessonComponent } from './inputs-lesson.component';
 
 @NgModule({
   declarations: [InputsLessonComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MatInputModule],
+  providers: [
+    { provide: MAT_PLACEHOLDER_GLOBAL_OPTIONS, useValue: { float: 'never' } }
+  ]
 })
 export class InputsLessonModule {}
