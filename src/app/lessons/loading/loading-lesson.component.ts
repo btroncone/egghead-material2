6a8f81d1fd732a91e0f0ee1eaffbb03b103ad892
btroncone <btroncone@gmail.com>
2017-11-25 11:44:09 -0500

    feat(loading): material loading examples

--- src/app/lessons/loading/loading-lesson.component.ts
+++ src/app/lessons/loading/loading-lesson.component.ts
@@ -1,21 +1,58 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material';
-
-export class CustomErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(
-    control: FormControl | null,
-    form: FormGroupDirective | NgForm | null
-  ): boolean {
-    return !!(control && control.invalid && control.dirty);
-  }
-}
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  transition,
+  style,
+  animate
+} from '@angular/animations';
+import { Subject } from 'rxjs/Subject';
+import { interval } from 'rxjs/observable/interval';
+import { concat } from 'rxjs/observable/concat';
+import { takeWhile, map, take, tap, ignoreElements } from 'rxjs/operators';
 
 @Component({
   selector: 'egm-loading-lesson',
   templateUrl: './loading-lesson.component.html',
+  animations: [
+    trigger('fadeOut', [
+      state('in', style({ opacity: 1 })),
+      transition('* => void', [
+        animate(
+          `1s .5s ease-out`,
+          style({
+            opacity: 0
+          })
+        )
+      ])
+    ])
+  ],
   styleUrls: ['./loading-lesson.component.scss']
 })
 export class LoadingLessonComponent implements OnInit {
-  ngOnInit() {}
+  public loadingPercent = 0;
+  public currentPlayback = 0;
+  public queryValue = 0;
+  public queryMode = 'query';
+  // public resetSpinners = new Subject<boolean>();
+
+  ngOnInit() {
+    this.loadingProgress(500).subscribe(i => (this.loadingPercent = i));
+
+    this.loadingProgress(250).subscribe(i => (this.currentPlayback = i));
+
+    // wait 5 seconds then switch mode to determinate, simulating waiting for response then download
+    concat(
+      interval(5000).pipe(
+        take(1),
+        tap(_ => (this.queryMode = 'determinate')),
+        ignoreElements()
+      ),
+      this.loadingProgress(500)
+    ).subscribe(i => (this.queryValue = i));
+  }
+
+  loadingProgress(speed: number) {
+    return interval(speed).pipe(map(i => i * 10), takeWhile(i => i <= 100));
+  }
 }
