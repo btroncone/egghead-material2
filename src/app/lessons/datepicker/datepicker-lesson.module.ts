3f9923cc9bf0f9b47b0d6cdcfef775820012f166
btroncone <btroncone@gmail.com>
2017-12-10 18:56:30 -0500

    feat(datepicker): updated datepicker lesson with examples

--- src/app/lessons/datepicker/datepicker-lesson.module.ts
+++ src/app/lessons/datepicker/datepicker-lesson.module.ts
@@ -1,10 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatDatepickerModule } from '@angular/material';
+import {
+  MatDatepickerModule,
+  MatInputModule,
+  MatButtonModule,
+  MatNativeDateModule
+} from '@angular/material';
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter
+} from '@angular/material-moment-adapter';
+import { PlatformModule } from '@angular/cdk/platform';
 import { DatepickerLessonComponent } from './datepicker-lesson.component';
 
 @NgModule({
   declarations: [DatepickerLessonComponent],
-  imports: [CommonModule, MatDatepickerModule]
+  imports: [
+    CommonModule,
+    PlatformModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatButtonModule
+  ]
 })
 export class DatepickerLessonModule {}
